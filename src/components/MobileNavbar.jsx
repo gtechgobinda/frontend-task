c20adf2781ccce74555527be8a60ef8e11ec9c2c
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-01-29 21:47:18 +0530

    some update

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -3,19 +3,29 @@ import { VscThreeBars } from "react-icons/vsc";
 import { IoIosSearch } from "react-icons/io";
 import { FaRegHeart } from "react-icons/fa";
 import { PiHandbagLight } from "react-icons/pi";
-import  companyLogo  from "../assets/companyLogo.svg";
+import companyLogo from "../assets/companyLogo.svg";
+import styled from "styled-components";
 const MobileNavbar = () => {
   return (
     <>
-      <nav>
-        <VscThreeBars />
-        <img src={companyLogo} alt="" />
-        <IoIosSearch />
-        <FaRegHeart />
-        <PiHandbagLight />
-      </nav>
+      <Wrapper>
+        <div>
+          <VscThreeBars />
+          <img src={companyLogo} alt="" />
+          <section className="heading-section">
+            <span className="title">GARNET LANEE</span>
+            <span>Dazzling you Today, Tomorrow & Forever</span>
+          </section>
+
+          <IoIosSearch />
+          <FaRegHeart />
+          <PiHandbagLight />
+        </div>
+      </Wrapper>
     </>
   );
 };
+
+const Wrapper = styled.nav``;
 
 export default MobileNavbar;
