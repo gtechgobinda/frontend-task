5f6a838c44d45c54656f756e6a8ebd80744a5961
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-01-30 11:40:15 +0530

    navbar some portion completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import MobileNavbar from "./components/MobileNavbar";
 
-const Title=styled.h1`
+const Title = styled.h1`
   font-size: 15px;
-`
+`;
 const App = () => {
   return (
-    <>   
-      <MobileNavbar/>
+    <>
+      <MainWrapper>
+        <MobileNavbar />
+      </MainWrapper>
     </>
   );
 };
 
+const MainWrapper = styled.section`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
+  * {
+    font-family: "Inter", sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
 export default App;
--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { VscThreeBars } from "react-icons/vsc";
 import { IoIosSearch } from "react-icons/io";
-import { FaRegHeart } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
 import { PiHandbagLight } from "react-icons/pi";
 import companyLogo from "../assets/companyLogo.svg";
 import styled from "styled-components";
@@ -9,23 +9,65 @@ const MobileNavbar = () => {
   return (
     <>
       <Wrapper>
-        <div>
-          <VscThreeBars />
-          <img src={companyLogo} alt="" />
-          <section className="heading-section">
-            <span className="title">GARNET LANEE</span>
-            <span>Dazzling you Today, Tomorrow & Forever</span>
+        <div className="Mobile-nav-conatiner">
+          <section>
+            <VscThreeBars className="three-bars" size={25} />
           </section>
-
-          <IoIosSearch />
-          <FaRegHeart />
-          <PiHandbagLight />
+          <section className="heading-section-container">
+            <img src={companyLogo} alt="" />
+            <section className="heading-section">
+              <span className="title">GARNET LANEE</span>
+              <span className="sub-title">
+                Dazzling you Today, Tomorrow & Forever
+              </span>
+            </section>
+          </section>
+          <section className="left-icons">
+            <IoIosSearch size={20} />
+            <CiHeart size={22} />
+            <PiHandbagLight size={20} />
+          </section>
         </div>
       </Wrapper>
     </>
   );
 };
 
-const Wrapper = styled.nav``;
+const Wrapper = styled.nav`
+  .Mobile-nav-conatiner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 12px;
+    padding-block: 20px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+    .heading-section-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 5px;
+      img {
+        width: 34px;
+      }
+      .heading-section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .title {
+          font-size: 25px;
+          font-weight:500;
+        }
+        .sub-title {
+          font-size: 9.5px;
+        }
+      }
+    }
+    .left-icons{
+        display: flex;
+        gap: 5px;
+    }
+  }
+`;
 
 export default MobileNavbar;
