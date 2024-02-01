71482b591d851cefe130c8b68a20c97add8a5ec4
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-02-01 10:51:14 +0530

    completed

--- src/components/ProductInformation.jsx
+++ src/components/ProductInformation.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { ProductInfoWrapper } from "../styles/ProductInformation.styles";
+import InfoLogo from "../assets/productInfo.svg";
 
 const ProductInformation = () => {
   return (
     <ProductInfoWrapper>
-      <section>
-        <div>ProductInformation</div>
+      <section className="main-product-info">
+        <div className="product-info-heading">
+          <img src={InfoLogo} alt="info" />
+          <span>Product Information:</span>
+        </div>
+        <div className="dimension-wight-purity">
+          <section className="dimension">
+            <p className="dimension-heading">Dimension</p>
+            <p>Width - 4.80 mm</p>
+            <p>Height - 2.30 mm</p>
+            <p>Size - 12(51.8 mm)</p>
+          </section>
+
+          <section className="wight-purity">
+            <p className="wight-heading">Weight</p>
+            <p>Approx.Weight - 3.920 Gm</p>
+            <p className="purity-heading">Purity</p>
+            <p>Purity - 18 KT</p>
+          </section>
+        </div>
+        <div className="diamonds-gemstones">
+          <p className="heading">Diamonds & Gemstones</p>
+          <section>
+            <span>
+              <p>Diamond Type</p>
+              <p>Setting Type</p>
+              <p>Total Number</p>
+              <p>Total Weight</p>
+            </span>
+            <span>
+              <p>IJ-SI</p>
+              <p>Micro Prong</p>
+              <p>56</p>
+              <p>0.22ct</p>
+            </span>
+          </section>
+        </div>
       </section>
     </ProductInfoWrapper>
   );
--- src/styles/ProductInformation.styles.jsx
+++ src/styles/ProductInformation.styles.jsx
@@ -1,7 +1,60 @@
 import styled from "styled-components";
 
 export const ProductInfoWrapper = styled.section`
-  section {
-    padding: 0px 20px;
+  .main-product-info {
+    padding: 15px 20px;
+    .product-info-heading {
+      display: flex;
+      gap: 5px;
+      font-size: 13px;
+      font-weight: 600;
+      color: #f77433;
+      margin-bottom: 10px;
+    }
+    .dimension-wight-purity {
+      display: flex;
+      flex-direction: row;
+      gap: 40px;
+      .dimension {
+        display: flex;
+        flex-direction: column;
+        font-size: 9px;
+        font-weight: 400;
+        color: #302f2f;
+        .dimension-heading {
+          font-size: 11px;
+          font-weight: 700;
+          margin-bottom: 5px;
+        }
+      }
+      .wight-purity {
+        font-size: 9px;
+        font-weight: 400;
+        border-left: 1px solid #e2e2e2;
+        padding-left: 10px;
+        .wight-heading {
+          font-size: 11px;
+          font-weight: 700;
+        }
+        .purity-heading {
+          font-size: 11px;
+          font-weight: 700;
+        }
+      }
+    }
+    .diamonds-gemstones {
+      margin-top: 10px;
+      .heading {
+        font-size: 13px;
+        font-weight: 700;
+      }
+      section {
+        font-size: 9px;
+        font-weight: 400;
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+      }
+    }
   }
 `;
